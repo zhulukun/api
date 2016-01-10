d741c09bfcc8ed10104dc0b2c0f68cb9d99e1ec6
zlk <1770195221@qq.com>
2016-01-10 15:41:06 +0800

    update

--- application/models/FriendInfo_model.php
+++ application/models/FriendInfo_model.php
@@ -264,7 +264,7 @@
 
     //获取汉字的第一个字母
     function _getFirstCharter($str){  
-      $encode = mb_detect_encoding($str, array("ASCII",'UTF-8′,"GB2312′,"GBK",'BIG5′)); 
+      $encode = mb_detect_encoding($str, array("ASCII","UTF-8","GB2312","GBK","BIG5")); 
       echo $str.'     '.$encode;
 if(empty($str)){return 'ZZZ';}  
 $fchar=ord($str{0});  
