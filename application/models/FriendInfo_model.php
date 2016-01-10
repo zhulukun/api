3d5ed29b6917950f6271aabc00e20bc204d4262a
zlk <1770195221@qq.com>
2016-01-10 15:32:13 +0800

    update

--- application/models/FriendInfo_model.php
+++ application/models/FriendInfo_model.php
@@ -267,10 +267,11 @@
 if(empty($str)){return 'ZZZ';}  
 $fchar=ord($str{0});  
 if($fchar>=ord('A')&&$fchar<=ord('z')) return strtoupper($str{0});  
-// $s1=iconv('UTF-8','gb2312',$str);  
-// $s2=iconv('gb2312','UTF-8',$s1);
-// $s=$s2==$str?$s1:$str;  
-$asc=ord($str{0})*256+ord($str{1})-65536;  
+$str=iconv('GBK','UTF-8',$str); 
+$s1=iconv('UTF-8','gb2312',$str);  
+$s2=iconv('gb2312','UTF-8',$s1);
+$s=$s2==$str?$s1:$str;  
+$asc=ord($s{0})*256+ord($s{1})-65536;  
 if($asc>=-20319&&$asc<=-20284) return 'A';  
 if($asc>=-20283&&$asc<=-19776) return 'B';  
 if($asc>=-19775&&$asc<=-19219) return 'C';  
